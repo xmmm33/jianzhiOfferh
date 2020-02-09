6943844891108d6a55dd001a4d0f55e65cac9654
xmmm33 <782980856@qq.com>
2020-02-09 14:37:19 +0800

    斐波拉切数列

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
+/**
+ * jianzhioffer-day1
+ * @author MSI
+ */
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(Fibonacci(7));
+    }
+
+    /**
+     * 斐波拉切数列 0 1 1 2 3 5 8 13
+     * @param n
+     * @return
+     */
+    public static int Fibonacci(int n) {
+        if(n == 0) {
+            return 0;
+        } else if(n == 1) {
+            return 1;
+        } else {
+            return Fibonacci(n-1)+Fibonacci(n-2);
+        }
     }
 }
