55718f04139a6bf0ffdbb6180e8023dd0c20a7c2
xmmm33 <782980856@qq.com>
2020-02-09 15:09:01 +0800

    斐波拉契数列非递归实现

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -5,21 +5,47 @@
 public class Fibonacci {
 
     public static void main(String[] args) {
-        System.out.println(Fibonacci(13));
+        System.out.println(Fibtwo(13));
     }
 
     /**
+     * 递归实现
      * 斐波拉切数列 0 1 1 2 3 5 8 13
+     * 当n很大的时候，会有很多的重复计算
      * @param n
      * @return
      */
-    public static int Fibonacci(int n) {
+    private static int Fibonacci(int n) {
         if(n == 0) {
             return 0;
         } else if(n == 1) {
             return 1;
         } else {
             return Fibonacci(n-1)+Fibonacci(n-2);
+        }
+    }
+
+    /**
+     * 循环实现
+     * 斐波拉切数列
+     * @param n
+     * @return
+     */
+    private static int Fibtwo(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            int ret = 0;
+            int a = 1;
+            int b = 0;
+            for (int i = 0; i < n - 1; i++) {
+                ret = a + b;
+                b = a;
+                a = ret;
+            }
+            return ret;
         }
     }
 }
