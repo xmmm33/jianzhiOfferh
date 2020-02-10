3ee209ed221395518c2e8fdbd19abb006569e681
xmmm33 <782980856@qq.com>
2020-02-10 16:52:35 +0800

    fix

--- src/day1/time_0209_2009/Solution.java
+++ src/day1/time_0209_2009/Solution.java
@@ -52,33 +52,7 @@ public class Solution {
         return 0;
     }
 
-    /**
-     * 二分法版本2
-     * @param array
-     * @return
-     */
-    public static int binarySearchTwo(int [] array) {
-        int low = 0;
-        int high = array.length - 1;
-        if (array.length <= 0) {
-            return 0;
-        }
-        // 用mid的值分别减去左右两边的值
-        // 往绝对值大的一边偏
-        while (low <= high) {
-            int mid = (low + high) >> 1;
-            if(Math.abs(array[mid] - array[high]) > Math.abs(array[mid] - array[low])){
-                low = mid + 1;
-            } else if (Math.abs(array[mid] - array[high]) < Math.abs(array[mid] - array[low])) {
-                high = mid - 1;
-            } else {
-                return array[mid];
-            }
-        }
-        return 0;
-    }
-
     public static void main(String[] args) {
-        System.out.println(binarySearch(new int[]{3,4,5,1,2}));
+        System.out.println(binarySearch(new int[]{6501,6828,6963,7036,7422,7674,8146,8468,8704,8717,9170,9359,9719,9895,9896,9913,9962,154,293,334,492,1323,1479,1539,1727,1870,1943,2383,2392,2996,3282,3812,3903,4465,4605,4665,4772,4828,5142,5437,5448,5668,5706,5725,6300,6335}));
     }
 }
