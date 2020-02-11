7b5f55634bbb7ac2253c6e930bc229688db9d159
xmmm33 <782980856@qq.com>
2020-02-11 15:07:38 +0800

    剑指Offer---倒序打印单链表

---
+++ src/day1/time_0211_1420/printListFromTailToHead.java
@@ -0,0 +1,76 @@
+package day1.time_0211_1420;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Stack;
+
+/**
+ * 从尾到头打印链表
+ *
+ */
+public class printListFromTailToHead {
+
+    public static class ListNode {
+        Integer val;
+        ListNode next = null;
+
+        ListNode(Integer val) {
+            this.val = val;
+        }
+    }
+
+    /**
+     * 带头结点的单链表的创建
+     * 头插法
+     * @param array
+     * @return
+     */
+    public static ListNode formLianbiao(int[] array) {
+        ListNode head = new ListNode(null);
+        ListNode header = head;
+        for (int i = 0; i <array.length ; i++) {
+            ListNode temp = new ListNode(array[i]);
+            temp.next = head.next;
+            head.next = temp;
+        }
+        return header;
+    }
+
+    /**
+     * 尾插法构建单链表
+     * @param array
+     * @return
+     */
+    public static ListNode formLianbiaoTwo(int[] array) {
+        ListNode head,storage,last;
+        head = new ListNode(null);
+        storage = last = null;
+        for (int i = 0; i < array.length; i++) {
+            last = new ListNode(array[i]);
+            if (i == 0) {
+                head.next = storage = last;
+            }else {
+                storage.next = last;
+                storage = last;
+            }
+        }
+        return head;
+    }
+
+    public static ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+        ArrayList<Integer> ret = new ArrayList<>();
+        ListNode node = listNode.next;
+        while (node != null) {
+            ret.add(0,node.val);
+            node = node.next;
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> integers = printListFromTailToHead(formLianbiaoTwo(new int[]{5, 6, 8, 5, 54, 23, 54}));
+        for (Integer i : integers) {
+            System.out.println(i);
+        }
+    }
+}
