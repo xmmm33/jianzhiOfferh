4948029b61c4c3f3c8973dbae83beb8ae001d304
xmmm33 <782980856@qq.com>
2020-02-26 11:08:09 +0800

    剑指Offer---二叉树的层次遍历

---
+++ src/day2/time_0226_0945/PrintFromTopToBottom.java
@@ -0,0 +1,74 @@
+package day2.time_0226_0945;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 从上到下遍历二叉树
+ * 也就是层次遍历
+ */
+public class PrintFromTopToBottom {
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        ArrayList<Integer> ret = new ArrayList<>();
+        List<TreeNode> temp = new LinkedList();
+        temp.add(root);
+        while (!temp.isEmpty()) {
+            TreeNode tempnode = ((LinkedList<TreeNode>) temp).poll();
+            ret.add(tempnode.val);
+            if (tempnode.left != null) {
+                temp.add(tempnode.left);
+            }
+            if (tempnode.right != null) {
+                temp.add(tempnode.right);
+            }
+        }
+        return ret;
+    }
+
+    /**
+     * 从下往上层次遍历二叉树
+     * @param root
+     * @return
+     */
+    public ArrayList<List<Integer>> PrintFromBottomToTop(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        ArrayList<List<Integer>> ret = new ArrayList<>();
+        LinkedList<TreeNode> templist = new LinkedList<>();
+        List<Integer> levelList = new ArrayList<>();
+        templist.add(root);
+        int levelSize = templist.size();
+        while (!templist.isEmpty()) {
+            if (levelSize == 0) {
+                ret.add(0, levelList);
+                levelSize = templist.size();
+                levelList = new ArrayList<>();
+            }
+            TreeNode node = templist.poll();
+            levelList.add(node.val);
+            if (node.left != null) {
+                templist.add(node.left);
+            }
+            if (node.right != null) {
+                templist.add(node.right);
+            }
+        }
+        ret.add(0, levelList);
+        return ret;
+    }
+}
