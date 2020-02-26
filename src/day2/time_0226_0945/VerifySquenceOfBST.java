097d3970d56715a79e330086d5d4a92a824069d5
xmmm33 <782980856@qq.com>
2020-02-26 17:29:44 +0800

    fix

--- src/day2/time_0226_0945/VerifySquenceOfBST.java
+++ src/day2/time_0226_0945/VerifySquenceOfBST.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 /**
  * 二叉搜索树的后序遍历
  * 输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历结果
- *
+ * 二叉搜索树的后序遍历满足：左子树《根节点《右子树
  */
 public class VerifySquenceOfBST {
 
@@ -24,14 +24,17 @@ public class VerifySquenceOfBST {
                 break;
             }
         }
+        // 如果index 为 -1 就说明这是一个递减的序列，相当于是一个单链表降序的二叉树 所以满足后序遍历条件 直接返回true
         if (index == -1) {
             return true;
         }
+        // 判断index后面的元素是否还有比根节点小的节点，如果有则不满足
         for (int i = index; i < sequence.length; i++) {
             if (sequence[i] < root) {
                 return false;
             }
         }
+        // 递归判断左右子树是否满足条件
         boolean left = VerifySquenceOfBST(Arrays.copyOf(sequence, index+1));
         boolean right = VerifySquenceOfBST(Arrays.copyOfRange(sequence, index, sequence.length-1));
         return left && right;
