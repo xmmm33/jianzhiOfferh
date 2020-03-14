7fee3cbbb21f5a6c44255b4bf877eeaf8ff9e88f
xmmm33 <782980856@qq.com>
2020-03-14 14:15:22 +0800

    希尔排序连续，希尔排序就是插入排序的升级版，多了一层定义步长的循环而已

---
+++ src/practice/sort/shell.java
@@ -0,0 +1,31 @@
+package practice.sort;
+
+public class shell {
+
+    public static void shell(int[] array) {
+        if (array.length < 1) {
+            return;
+        }
+        // 定义步长
+        for (int gap = array.length / 2; gap > 0; gap = gap / 2) {
+            // 找打步长所在位置元素
+            for (int i = gap; i < array.length; i = i + gap) {
+                // 跟步长所在以前的元素开始比较
+                for (int j = i; j >= gap; j = j - gap) {
+                    if (array[j] < array[j - gap]) {
+                        int temp = array[j - gap];
+                        array[j - gap] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+        }
+        for (int i : array) {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String[] a) {
+        shell(new int[]{1,5,6,1,32,5,4,5,1,2,54,3,131});
+    }
+}
