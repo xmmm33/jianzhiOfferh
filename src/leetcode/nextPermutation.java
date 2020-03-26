966c1d8444d6fabe796ba88f28ebea67021b7505
xmmm33 <782980856@qq.com>
2020-03-26 18:54:43 +0800

    LeetCode4

---
+++ src/leetcode/nextPermutation.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class nextPermutation {
+
+    public static void nextPermutation(int[] nums) {
+        int len = nums.length;
+        for (int i = nums.length-1; i >=0 ; i--) {
+            if (i == 0) {
+                Arrays.sort(nums);
+            }else {
+                if (nums[i] > nums[i - 1]) {
+                    int tempindex = i-1;
+                    for (int j = nums.length-1; j >=0 ; j--) {
+                        if (nums[j] > nums[tempindex]) {
+                            int temp = nums[j];
+                            nums[j] = nums[tempindex];
+                            nums[tempindex] = temp;
+                            Arrays.sort(nums,tempindex+1,len);
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        nextPermutation(new int[]{1, 2, 3});
+    }
+}
---
+++ src/leetcode/threecolor.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+public class threecolor {
+
+    public static void three(int[] array) {
+        int w =0;
+        int b = 0;
+        int r = array.length - 1;
+        while (w <= r) {
+            if (array[w] == -1) {
+                w++;
+            } else if (array[w] == 1) {
+                int temp = array[b];
+                array[b] = array[w];
+                array[w] = temp;
+                w++;
+                b++;
+            } else if (array[w] == 0) {
+                int temp = array[r];
+                array[r] = array[w];
+                array[w] = temp;
+                r--;
+            }
+        }
+        for (int i : array) {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        three(new int[]{-1, -1, 0, 0, 1, 1});
+    }
+}
