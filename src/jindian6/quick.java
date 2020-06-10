4b025bfe2f19a94526ad5b030fbb37ba47a5f8ef
xmmm <782980856@qq.com>
2020-06-10 22:55:31 +0800

    "aaa"

---
+++ src/jindian6/quick.java
@@ -0,0 +1,39 @@
+package jindian6;
+
+public class quick {
+
+
+    public static void main(String[] args) {
+        quicksort(new int[]{1, 5, 6, 3, 1, 8, 4});
+    }
+
+    public static void quicksort(int[] nums) {
+        if (nums == null || nums.length < 1) {
+            return;
+        }
+        quicks(nums, 0, nums.length - 1);
+        System.out.println(111);
+    }
+
+    private static void quicks(int[] nums, int low, int high) {
+        if (low >= high) {
+            return;
+        }
+        int base = nums[low];
+        int l = low;
+        int r = high;
+        while (l < r) {
+            while (l < r && base <= nums[r]) {
+                r--;
+            }
+            nums[l] = nums[r];
+            while (l < r && base >= nums[l]) {
+                l++;
+            }
+            nums[r] = nums[l];
+        }
+        nums[l] = base;
+        quicks(nums, low, l - 1);
+        quicks(nums, l + 1, high);
+    }
+}
